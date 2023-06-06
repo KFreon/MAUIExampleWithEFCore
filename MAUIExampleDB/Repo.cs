b2d6b63f8f179f6947ee00fcbe13d37f48cfb347
Kael Larkin <kael.larkin@purple.telstra.com>
2023-06-06 19:34:25 +1000

    Whoops, add all the files in the right place

---
+++ MAUIExampleDB/Repo.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MAUIExampleDB
+{
+    public class MyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public interface IRepo
+    {
+        DbSet<MyModel> Models { get; set; }
+
+        Task MigrateAsync();
+        Task SaveChangesAsync();
+    }
+
+    public class Repo : DbContext, IRepo
+    {
+        public Repo(DbContextOptions<Repo> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MyModel> Models { get; set; }
+
+        public async Task MigrateAsync()
+        {
+            await base.Database.MigrateAsync();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MyModel>()
+                .HasKey(x => x.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MAUIExampleDBEntryPoint/Program.cs
@@ -0,0 +1,13 @@
+﻿using MAUIExampleDB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+Console.WriteLine("This is the efcore CLI migrations entrypoint. Use 'dotnet ef migrations add <migration name> --project MAUIExampleDB/MAUIExampleDB.csproj --startup-project MAUIExampleDBEntryPoint/MAUIExampleDBEntryPoint.csproj' to add a new migration");
+
+public class DesignRepo : IDesignTimeDbContextFactory<Repo>
+{
+    public Repo CreateDbContext(string[] args)
+    {
+        return new Repo(new DbContextOptionsBuilder<Repo>().UseSqlite("doesnt matter").Options);
+    }
+}
