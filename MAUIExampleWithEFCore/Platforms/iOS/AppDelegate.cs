4e2ac82f743cc033ffde8e82e3a4551d976fe796
Kael Larkin <kael.larkin@purple.telstra.com>
2023-06-06 19:34:59 +1000

    Remove unnecessary platform files

--- MAUIExampleWithEFCore/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MAUIExampleWithEFCore;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
